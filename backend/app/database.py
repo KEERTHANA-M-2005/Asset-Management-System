d76e3ecd32c428ef20df8b71aad1be446a7a6755
KEERTHANA-M-2005 <keerthigowli05@gmail.com>
2026-04-12 16:28:12 +0530

    Updated database and models with working api's

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./assettrack.db"
+DATABASE_URL = "postgresql://postgres:Postgre@localhost:5432/assettrackpro_db"
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False
 )
-
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
--- backend/app/models.py
+++ backend/app/models.py
@@ -6,7 +6,7 @@ from .database import Base
 class Role(Base):
     __tablename__ = "roles"
 
-    role_id = Column(Integer, primary_key=True, index=True)
+    role_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     role_name = Column(String, unique=True)
     permissions = Column(String)
 
@@ -14,7 +14,7 @@ class Role(Base):
 class User(Base):
     __tablename__ = "users"
 
-    user_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     username = Column(String, unique=True)
     password = Column(String)
 
@@ -25,7 +25,7 @@ class User(Base):
 class Employee(Base):
     __tablename__ = "employees"
 
-    employee_id = Column(Integer, primary_key=True, index=True)
+    employee_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     employee_name = Column(String, nullable=False)
     department = Column(String, nullable=False)
 
@@ -33,7 +33,7 @@ class Employee(Base):
 class Asset(Base):
     __tablename__ = "assets"
 
-    asset_id = Column(Integer, primary_key=True, index=True)
+    asset_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     asset_name = Column(String, nullable=False)
     asset_status = Column(String, default="AVAILABLE")
 
@@ -41,13 +41,12 @@ class Asset(Base):
 class Assignment(Base):
     __tablename__ = "assignments"
 
-    assignment_id = Column(Integer, primary_key=True, index=True)
+    assignment_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     asset_id = Column(Integer, ForeignKey("assets.asset_id"), nullable=False)
     employee_id = Column(Integer, ForeignKey("employees.employee_id"), nullable=False)
     assigned_date = Column(DateTime, nullable=False)
     returned_date = Column(DateTime, nullable=True)
-    status = Column(String, default="ASSIGNED")  # ASSIGNED or RETURNED
+    status = Column(String, default="ASSIGNED")
 
-    # Relationships
     asset = relationship("Asset")
     employee = relationship("Employee")
